cc38188cc89764ffa4162ae250a789b0e35aa81e
CARLOS JAVIER GALAN RODRIGUEZ <1088279@est.intec.edu.do>
2021-10-01 23:00:17 -0400

    Added all the pages to views.

--- ChefingApp/ChefingApp/ViewModels/SearchRecipesViewModel.cs
+++ ChefingApp/ChefingApp/ViewModels/SearchRecipesViewModel.cs
@@ -1,14 +1,45 @@
 ﻿using ChefingApp.Models;
 using ChefingApp.Services;
 using Prism.Commands;
+using Prism.Navigation;
 using Prism.Services;
 using System.Collections.ObjectModel;
 using Xamarin.Essentials;
+using Prism.Mvvm;
 
 namespace ChefingApp.ViewModels
 {
-    class SearchRecipesViewModel : BaseViewModel
+    public class SearchRecipesViewModel : BaseViewModel
     {
+        //private string _title;
+
+        //public string Title
+        //{
+        //    get { return _title; }
+        //    set { SetPriority(ref _title, value); }
+        //}
+
+        //private DelegateCommand _navigateCommand;
+        //private readonly INavigationService _navigationService;
+
+        //public DelegateCommand NavigateCommand =>
+        //    _navigateCommand ?? (_navigateCommand = new DelegateCommand(ExecuteNavigateCommand));
+
+
+
+        //public SearchRecipesViewModel(INavigationService navigationService)
+        //{
+        //    Title = "Main Page";
+        //    _navigationService = navigationService;
+        //}
+
+        //async void ExecuteNavigateCommand()
+        //{
+        //    await _navigationService.NavigateAsync("");
+        //}
+
+
+
         public string SearchString { get; set; }
         public ObservableCollection<RecipeHits> RecipesCollection { get; set; }
 
@@ -39,7 +70,8 @@ namespace ChefingApp.ViewModels
 
            
         }
+ 
 
 
-}
+    }
 }
