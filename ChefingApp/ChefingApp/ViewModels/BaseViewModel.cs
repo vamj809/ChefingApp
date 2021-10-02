c1412c3411955035fc2fa7a3cbeae492e5684f5a
Víctor A. Montero <vamj809@users.noreply.github.com>
2021-10-01 21:20:34 -0400

    removed propertychanged warnings

--- ChefingApp/ChefingApp/ViewModels/BaseViewModel.cs
+++ ChefingApp/ChefingApp/ViewModels/BaseViewModel.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 
 namespace ChefingApp.ViewModels
 {
+    [SuppressPropertyChangedWarnings]
     public abstract class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
