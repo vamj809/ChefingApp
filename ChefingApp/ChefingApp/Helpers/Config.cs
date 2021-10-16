4648867ac5113f3c449f27d75d952a444132f20e
CARLOS JAVIER GALAN RODRIGUEZ <1088279@est.intec.edu.do>
2021-10-16 02:01:36 -0400

    Deleted RecipeDetailsPage and Added the icons

--- ChefingApp/ChefingApp/Helpers/Config.cs
+++ ChefingApp/ChefingApp/Helpers/Config.cs
@@ -8,6 +8,6 @@ namespace ChefingApp.Helpers
     {
         public const string ApiBaseUrl = "https://api.edamam.com/api/recipes/v2";
         public const string ApiApplicationID = "8acfdcef";
-        public const string ApiAccessKey = "4dd302e945e4367ca82ce0640171448c";
+        public const string ApiAccessKey = "YOUR CODE HERE";
     }
 }
